a49eea366f0e59d1a12a9dd23e94ec7b37a6bed3
nasirshad <muhammadnasirshad96@gmail.com>
2023-09-02 11:50:22 +0500

    Message

---
+++ Q9.js
@@ -0,0 +1,26 @@
+"use strict";
+// Q No.9.	Develop a program that determines the day of the week. 
+// Ask the user for a number (1-7) and use nested
+// if statements to print the corresponding day's name.
+var day = 2;
+if (day == 1) {
+    console.log("Monday");
+}
+else if (day == 2) {
+    console.log("Tuesday");
+}
+else if (day == 3) {
+    console.log("Wednesday");
+}
+else if (day == 4) {
+    console.log("Thursday");
+}
+else if (day == 5) {
+    console.log("Friday");
+}
+else if (day == 6) {
+    console.log("Satureday");
+}
+else {
+    console.log("Sunday");
+}
