bfb38109eea7a13b943edf3962df0f562585eb67
nasirshad <muhammadnasirshad96@gmail.com>
2023-09-02 11:46:37 +0500

    Message

---
+++ Q8.js
@@ -0,0 +1,10 @@
+"use strict";
+// Q No.8.	Write a program that checks if the given year 
+// is leap year or not.
+var year = 2018;
+if (year % 4 == 0) {
+    console.log("The year", year, " is Leap Year");
+}
+else {
+    console.log("The year", year, "is not a Leap year");
+}
---
+++ Q8.ts
@@ -0,0 +1,11 @@
+// Q No.8.	Write a program that checks if the given year 
+// is leap year or not.
+var year:number=2018;
+if (year%4==0) 
+{
+    console.log("The year" ,year, " is Leap Year")    
+} 
+else 
+{
+    console.log("The year", year, "is not a Leap year")    
+}
