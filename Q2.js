85f05d1394bbd2a8ae577565ec04a93a49e29857
nasirshad <muhammadnasirshad96@gmail.com>
2023-09-02 11:20:24 +0500

    Message

---
+++ Q2.js
@@ -0,0 +1,6 @@
+"use strict";
+// Q2 - Write a program that calculates the percentage.
+var total = 1100;
+var obt = 640;
+var perc = (obt / total) * 100;
+console.log("The Percentage of the given number is =", perc);
---
+++ Q2.ts
@@ -0,0 +1,5 @@
+// Q2 - Write a program that calculates the percentage.
+var total:number=1100
+var obt:number=640
+var perc:number=(obt/total)*100
+console.log ("The Percentage of the given number is =", perc)
